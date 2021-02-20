928aabbe0c7073915cd2aa3b22c24b5292c572d9
YuHua <yuhua@dgg.net>
2021-02-20 11:21:27 +0800

    首页使用DefaultTabController+TabBar+TabBarView组合

--- sports/lib/Home/Home.dart
+++ sports/lib/Home/Home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:sports/Home/Bottom/Bottom.dart';
 import 'package:sports/Home/CenterList/CenterList.dart';
 import 'package:sports/Home/Top/Top.dart';
-import 'package:sports/Home/TopType/TopType.dart';
 import 'package:sports/Tools/ColorTools.dart';
 
 class Home extends StatefulWidget {
@@ -12,43 +11,70 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _types = ["所有", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲"];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorF9F9F9,
-      body: SafeArea(
-        child: Column(
-          children: [
-            /// 顶部
-            HomeTop((index) {
+    return DefaultTabController(
+      length: _types.length,
+      child: Scaffold(
+        backgroundColor: ColorF9F9F9,
+        appBar: AppBar(
+          flexibleSpace: SafeArea(
+            child: HomeTop((index) {
               print("切换TOP分类:$index");
             }, () {
               print("点击了搜索按钮");
             }, currentIndex: 0,),
-            SizedBox(height: 15,),
-            /// 顶部比赛分类
-            TopType(
-                ["所有", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲", "西甲"],
-                    (index) {
-                  print("点击了分类比赛:$index");
-            }, 0
-            ),
-            /// 列表
-            Expanded(
-              child: CenterList(),
-            ),
-            /// 底部
-            Bottom(
-                0,
-                (index) {
-                  print("点击了BOTTOM分类:$index");
-                },
-                () {
-                  print("点击了刷新按钮");
-                }
-            ),
-          ],
+          ),
+          backgroundColor: ColorF9F9F9,
+          bottom: _buildType()
         ),
+        body: TabBarView(
+          children: _types.map((e) => _buildContent()).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildType() {
+    return TabBar(
+      tabs: _types.map((e) => Tab(text: e,)).toList(),
+      isScrollable: true,
+      indicatorColor: Color27C5C3,
+      labelColor: Color27C5C3,
+      unselectedLabelColor: Color666666,
+      labelStyle: TextStyle(
+          color: Color27C5C3,
+          fontSize: 14,
+          fontWeight: FontWeight.bold
+      ),
+      unselectedLabelStyle: TextStyle(
+          color: Color666666,
+          fontSize: 14,
+          fontWeight: FontWeight.normal
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return SafeArea(
+      child: Column(
+        children: [
+          /// 列表
+          Expanded(
+            child: CenterList(),
+          ),
+          /// 底部
+          Bottom(
+              0,
+                  (index) {
+                print("点击了BOTTOM分类:$index");
+              },
+                  () {
+                print("点击了刷新按钮");
+              }
+          ),
+        ],
       ),
     );
   }
